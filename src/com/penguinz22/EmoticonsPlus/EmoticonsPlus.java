41ed259f3201fb9c34cee78bd7b698d428e601ef
Penguinz22 <nate19522@gmail.com>
2020-05-17 17:26:20 -0700

    Formatting
    Added a space between Listener and {

--- src/com/penguinz22/EmoticonsPlus/EmoticonsPlus.java
+++ src/com/penguinz22/EmoticonsPlus/EmoticonsPlus.java
@@ -12,7 +12,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class EmoticonsPlus extends JavaPlugin implements Listener{
+public class EmoticonsPlus extends JavaPlugin implements Listener {
 
 	public List<Emote> emotes;
 	private EmoteConfiguration config;
